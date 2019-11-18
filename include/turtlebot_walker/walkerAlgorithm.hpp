2a168293750bf8d275743f4d863cd79d1e77fe7c
Arjung27 <arjun.g1511@gmail.com>
2019-11-17 23:26:07 -0500

    [include/turtlebot_walker/walkerAlgorithm.hpp] Adding the walkingAlgorithm.hpp the header file for the algorithm

---
+++ include/turtlebot_walker/walkerAlgorithm.hpp
@@ -0,0 +1,89 @@
+/**
+ *  MIT License
+ *
+ *  Copyright (c) 2019 Arjun Gupta
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without
+ *  limitation the rights to use, copy, modify, merge, publish, distribute,
+ *  sublicense, and/or sell copies of the Software, and to permit persons to
+ *  whom the Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included
+ *  in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ *@file walkerAlgorithm.hpp
+ *@author Arjun Gupta
+ *@copyright MIT License
+ *@brief declare walkerAlgorithm class functions
+ */
+
+#ifndef INCLUDE_TURTLEBOT_WALKER_WALKERALGORITHM_H_
+#define INCLUDE_TURTLEBOT_WALKER_WALKERALGORITHM_H_
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h"
+
+/**
+ * @brief      Class for walkerAlgorithm.
+ */
+class walkerAlgorithm {
+ private:
+  // Variable for velocities
+  geometry_msgs::Twist msg;
+  // node handler
+  ros::NodeHandle nh;
+  // publish velocities
+  ros::Publisher publishVelocity;
+  // subscribe laserscan topic
+  ros::Subscriber subscribeVelocity;
+  // variable for default linear speed
+  float linearVelocity;
+  // variable for default angular speed
+  float angularVelocity;
+  // Variable to detect collisions
+  bool collision;
+
+ public:
+  /**
+   * @brief  constructor for walkerAlgorithm
+   */
+  walkerAlgorithm();
+  /**
+   * @brief  default destructor to destroy object
+   */
+  ~walkerAlgorithm();
+  /**
+   * @brief  callback function to analyze laserScan data
+   * 
+   * @param  msg Pointer to messages from LaserScan
+   * @return void
+   */
+  void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+  /**
+   * @brief   Checks for obstacles nearby
+   * @return  Boolean type value. 1 if obstacle is found else 0.
+   */
+  bool checkObstacle();
+  /**
+   * @brief  function to navigate the robot
+   * @return void
+   */
+  void moveRobot();
+};
+
+#endif
